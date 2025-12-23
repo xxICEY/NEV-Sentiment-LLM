4cd6223b8c09a4fbd3469a14130f688aef408ff9
xxICEY <liuliansiji@gmail.com>
2025-12-23 11:45:10 +0800

    Create llm_agent.py

---
+++ src/llm_agent.py
@@ -0,0 +1,8 @@
+class LLMAgent:
+    def explain_topic(self, keywords):
+        """
+        输入：LDA 提取的关键词 (如: 电池, 衰减, 冬季)
+        输出：LLM 总结的主题名称 (如: 续航焦虑)
+        """
+        # TODO: 调用 DeepSeek 或 GPT-4 接口
+        pass
